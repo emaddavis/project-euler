9357ca79148101fbffd777c6c29389c24304b326
Emad Davis <emaddavis@gmail.com>
2020-12-04 16:18:47 -0500

    prob5 done

--- euler_ate.py
+++ euler_ate.py
@@ -1,4 +1,32 @@
 ################################################################################
+def prob4():
+    result = []
+    for i in range(1000):
+        for j in range(1000):
+            product = i*j
+            if is_palindrom(product):
+                result.append(product)
+    print(max(result))
+
+def is_palindrom(n):
+    result = True
+    n = str(n)
+    array = list(n)
+    length = int(len(array)/2)
+    count = 0
+    while count < length and result:
+        first = 0
+        last = -1
+
+        if (array[first+count] !=  array[last-count]):
+            result = False
+        
+        count += 1
+    
+    return result
+
+prob4()
+################################################################################
 def prob3():
     n = 600851475143
     x = 2
@@ -8,7 +36,7 @@ def prob3():
             n=n/x
         x += 1
     print(n)
-prob3()
+#prob3()
 ################################################################################
 def prob2():
     x, y = 1, 1
